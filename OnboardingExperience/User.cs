448fcea007464156714b1674a77d719958ed5cb2
Chris Finney <chrisfinney86@gmail.com>
2018-04-11 12:50:35 -0500

    most of the functionality completed

---
+++ OnboardingExperience/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace OnboardingExperience
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string LogInPinNum { get; set; }
+        public string IsAccount { get; set; }
+
+        //public User()
+        //{
+        //}
+
+        public User(string FName, string LName, string PIN, string hasAccount){
+                FirstName = FName;
+                LastName = LName;
+                LogInPinNum = PIN;
+                IsAccount = hasAccount;
+            }
+
+        }
+    }
